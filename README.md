e2f8648fc8eae28949823168b25a4c6043a0cd44
alex_korentsvit <alex.dota@gmail.com>
2017-12-10 15:25:11 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+A Task:
+There are n (nºN, n>1) balls.
+One of them differs from all others in weight.
+How to find this ball for the minimum number of weighings?
---
+++ find_ball.py
@@ -0,0 +1,59 @@
+import math
+    
+def FindBall(balls, num_iterations):
+    try:
+        if balls[0] == balls[1]:
+            print('We weigh the balls: ' + str(balls[0]) + ' and ' + str(balls[1]))
+    except:
+        pass
+        
+    for num_balls in balls:
+        
+        if num_iterations == 0:
+            print('NEXT ---->')
+            return True     
+        
+        print('The number of balls: ' + str(num_balls) + ', The number of remaining iteration: '  + str(int(num_iterations)))
+        
+        previous_degree = num_iterations - 1
+        divider = 3**previous_degree
+        num_dividers = num_balls // divider
+        rest = num_balls % divider
+        result = [int(divider)] * int(num_dividers)
+        
+        if not rest == 0:
+            result.append(rest)
+            
+        new_num_iterations = num_iterations-1
+        
+        print('Separation of balls: ' + str(result))
+        print('----------------------')      
+        
+        FindBall(result, new_num_iterations)
+
+
+
+while True:
+    num_balls = input('Enter the number of balls: ')
+    if type(num_balls) == int:
+        if num_balls > 1:
+            break
+        else:
+            print('the number of balls must be > 1\n')
+    else:
+        print('The number of balls must be natural number!!!\n')
+    
+        
+
+#Get number of min iterations
+num_iterations = math.log(num_balls,3)
+
+# Get the previous degree of 3
+if num_iterations % 3 == 0:
+    previous_degree = num_iterations - 1
+else:
+    previous_degree = int(str(num_iterations).split('.')[0])
+    num_iterations = previous_degree + 1
+    
+FindBall([num_balls], num_iterations)
+print('We find it!!! Minimum number of weighings: ' + str(int(num_iterations)))
