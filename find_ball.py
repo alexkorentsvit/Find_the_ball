f2aede8990345f70ae84f8054747fa8237908a6f
alex_korentsvit <alex.dota@gmail.com>
2017-12-10 15:41:34 +0200

    added comments

--- find_ball.py
+++ find_ball.py
@@ -1,6 +1,7 @@
 import math
     
 def FindBall(balls, num_iterations):
+    
     try:
         if balls[0] == balls[1]:
             print('We weigh the balls: ' + str(balls[0]) + ' and ' + str(balls[1]))
@@ -9,12 +10,14 @@ def FindBall(balls, num_iterations):
         
     for num_balls in balls:
         
+        # The end of recursion
         if num_iterations == 0:
             print('NEXT ---->')
             return True     
         
         print('The number of balls: ' + str(num_balls) + ', The number of remaining iteration: '  + str(int(num_iterations)))
         
+        # Math part
         previous_degree = num_iterations - 1
         divider = 3**previous_degree
         num_dividers = num_balls // divider
@@ -29,10 +32,11 @@ def FindBall(balls, num_iterations):
         print('Separation of balls: ' + str(result))
         print('----------------------')      
         
+        # Recursion
         FindBall(result, new_num_iterations)
 
 
-
+# Input
 while True:
     num_balls = input('Enter the number of balls: ')
     if type(num_balls) == int:
@@ -45,7 +49,7 @@ while True:
     
         
 
-#Get number of min iterations
+# Get number of min iterations
 num_iterations = math.log(num_balls,3)
 
 # Get the previous degree of 3
@@ -54,6 +58,8 @@ if num_iterations % 3 == 0:
 else:
     previous_degree = int(str(num_iterations).split('.')[0])
     num_iterations = previous_degree + 1
-    
+
+
+# Output    
 FindBall([num_balls], num_iterations)
 print('We find it!!! Minimum number of weighings: ' + str(int(num_iterations)))
